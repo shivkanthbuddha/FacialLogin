aa89beb797bceb4dcbdb989dd2e0065b7840f8a3
Shiv Buddha <shivakanthb@gmail.com>
2019-01-22 19:29:48 +0530

    able to do login functionality

--- sketch.js
+++ sketch.js
@@ -6,16 +6,18 @@ let ukeButton;
 let whistleButton;
 let trainButton;
 let personName;
+let msg;
 
 
 function modelReady() {
   console.log('Model is ready!!!');
   // classifier.load('model.json', customModelReady);
+  msg.html( 'I am Ready with Mobilenet');
 }
 
 function customModelReady() {
   console.log('Custom Model is ready!!!');
-  label = 'model ready';
+  msg.html(  'I am Ready with Custom model');
   classifier.classify(gotResults);
 }
 
@@ -33,6 +35,8 @@ function setup() {
   classifier = mobilenet.classification(video, videoReady);
   personName = select("#personName");
 
+  msg= select("#msg");  
+
   addAnImage = select("#addAnImage"); 
   addAnImage.mousePressed(function () {
     classifier.addImage( personName.value());
@@ -49,29 +53,42 @@ function setup() {
   });
 }
 
+function login(){
+  select("#buttons").hide();
+  select("#nameSection").hide()
+  trainButton.hide();
+  saveButton.hide();
+  classifier.classify(gotResults);
+}
+
+
 function draw() {
   background(0);
   image(video, 0, 0, 400, 400);
   fill(255);
   textSize(16);
-  text(label, 10, height - 10);
+ // text(label, 10, height - 10);
 }
 
 
 function whileTraining(loss) {
   if (loss == null) {
+    msg.html( "Registration Complete")
     console.log('Training Complete');
-    classifier.classify(gotResults);
+    //classifier.classify(gotResults);
   } else {
+    msg.html( "Please wait...Training in progress")
     console.log(loss);
   }
 }
+
 
 
 function gotResults(error, result) {
   if (error) {
     console.error(error);
   } else {
+    msg.html( "Hi " + result.toUpperCase() + " Welcome back ");
     label = result;
     classifier.classify(gotResults);
   }
